70301f591f7f3b5f07ff1de8e6c536aa145e85a7
FloMRI <florianmari@hotmail.fr>
2025-12-15 09:53:05 +0100

    Lesson 1 and 2

---
+++ src/Controller/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StarshipRepository;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function homepage(StarshipRepository $starshipRepository, HttpClientInterface $client, #[Autowire(service: 'twig.command.debug')] DebugCommand $twigDebugCommand): Response
+    {
+        $output = new BufferedOutput();
+        $twigDebugCommand->run(new ArrayInput([]), $output);
+        $ships = $starshipRepository->findAll();
+        $myShip = $ships[array_rand($ships)];
+
+        return $this->render('main/homepage.html.twig', [
+            'myShip' => $myShip,
+            'ships' => $ships
+        ]);
+    }
+}
---
+++ src/Repository/StarshipRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\Starship;
+use App\Model\StarshipStatusEnum;
+use Psr\Log\LoggerInterface;
+
+class StarshipRepository
+{
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
+    public function findAll(): array
+    {
+        $this->logger->info('Starship collection retrieved');
+
+        return [
+            new Starship(
+                1,
+                'USS LeafyCruiser (NCC-0001)',
+                'Garden',
+                'Jean-Luc Pickles',
+                StarshipStatusEnum::IN_PROGRESS,
+                new \DateTimeImmutable('2025-12-12')
+            ),
+            new Starship(
+                2,
+                'USS Espresso (NCC-1234-C)',
+                'Latte',
+                'James T. Quick!',
+                StarshipStatusEnum::COMPLETED,
+                new \DateTimeImmutable('2025-07-02')
+            ),
+            new Starship(
+                3,
+                'USS Wanderlust (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                StarshipStatusEnum::WAITING,
+                new \DateTimeImmutable('2025-02-03')
+            ),
+        ];
+    }
+
+    public function find(int $id): ?Starship
+    {
+        foreach ($this->findAll() as $starship) {
+            if ($starship->getId() === $id) {
+                return $starship;
+            }
+        }
+
+        return null;
+    }
+}
